80d815f887396583a26ca2e1618a64a55f79d91a
ZariaNYU <144282037+ZariaNYU@users.noreply.github.com>
2023-12-19 11:03:51 -0500

    Iteration

--- sketch.js
+++ sketch.js
@@ -14,6 +14,8 @@ let currentButton;
 let angle = 0;
 let recordImg;
 
+let stars = [];
+
 function connect() {
   mSerial.open(9600);
   readyToRead = true;
@@ -23,6 +25,7 @@ function connect() {
 function preload() {
 recordImg = loadImage('recordImg.jpg'); // preload images
 chromeImg = loadImage('chromeImg.png');
+starImg = loadImage('star.png');
 
 songs[0] = loadSound("./tainted.mp3"); // preload music
 songs[1] = loadSound("./watching.mp3");
@@ -33,7 +36,7 @@ recordScratch = loadSound("./recordScratch.mp3");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(800, 700, WEBGL);
   angleMode(DEGREES);
 
   mSerial = createSerial();
@@ -42,9 +45,9 @@ function setup() {
   isPlaying = false;
 
   currentButton = createButton('');
-  currentButton.position(width * 4 / 5, height / 6);
-  currentButton.size(300, 300);
-  currentButton.style('border-radius', '50%');
+  currentButton.position(width * 2 / 3, 0);
+  currentButton.size(200, 200);
+  currentButton.style('border-radius', '75%');
   currentButton.style('border-width', '0');
   currentButton.style('background-size', 'cover');
   currentButton.style('background-image', 'url(skipbutton.png)');
@@ -54,8 +57,19 @@ function setup() {
   mConnectButton = createButton("Connect to Serial");
   mConnectButton.position(0, 0);
   mConnectButton.size(mConnectButton.width, mConnectButton.height);
-  mConnectButton.style('font-size', ' 12px');
+  mConnectButton.style('font-size', ' px');
   mConnectButton.mousePressed(connect);
+
+  for (let i = 0; i < 50; i++) {
+    let star = {
+      x: random(-width, width),
+      y: random(-height, height),
+      size: random(10, 25),
+      color: color(random(0, 255), random(0, 255), random(0, 255)),
+      speed: random(1, 3)
+    };
+    stars.push(star);
+  }
 
 }
 
@@ -73,6 +87,19 @@ function changeSong(){
 
 function draw() {
   background(0);
+
+  for (let i = 0; i < stars.length; i++) {
+    let star = stars[i];
+    star.x -= star.speed;
+
+    if (star.x < -width / 2) {
+      star.x = width / 2;
+      star.y = random(-height / 2, height / 2);
+    }
+
+    image(starImg, star.x, star.y, star.size, star.size);
+  
+  }
   
   if (readyToRead && mSerial.opened()) {
     mSerial.clear();
@@ -110,9 +137,8 @@ function draw() {
 
   recordSpin();
   recordNeedle();
-  //skipButton();
-
-}
+  
+  }
 
 //function skipButton() {
 //texture(skipButton);
@@ -131,7 +157,7 @@ function recordSpin() {
   rotate(angle);
   texture(recordImg);
   let minDim = min(width, height);
-  circle( 0, 0, minDim / 1.5);
+  circle( 0, 0, minDim / 2);
   fill(0);
   circle(0, 0, minDim / 10);
   angle += 1;
@@ -140,35 +166,24 @@ function recordSpin() {
 }
 
 
-// draw record needle
-function recordNeedle() { // **FIX DIMENSIONS
+function recordNeedle() {
 texture(chromeImg);
 stroke(255);
-strokeWeight(6);
+strokeWeight(3);
 push();
-translate( width / 10, - height / 5);
+translate( 0, 0);
 
 if (!isPlaying) {
   rotate(-45);
 }
 
-circle(0, 0, 200);
+circle(width / 6, - height / 6, 50);
 
 stroke(255);
-strokeWeight(20);
-line(0, 0, - 600, 600);
+strokeWeight(5);
+line(width / 6, - height / 6, -20, 50);
 pop();
 }
-
-// moving stars across the screen
-//class starParticles {
-
- // constructor(){
- //   this.x = random(0, width);
- //   this.y = random(0, height);
- //   this.xSpeed = random()
- // }
-//}
 
 // function for audio visualizer
 
